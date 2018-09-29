643d66829ea35423e4a57c8482b38c1688937eff
Tobias Tefke <tobias.tefke@tutanota.com>
2018-09-29 15:18:23 +0200

    Use version 1.0 of qcom power service
    Oce has no wifi stats, so we need to use 1.0 HAL

--- device.mk
+++ device.mk
@@ -362,7 +362,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-service-qti
 
 # Prebuilts
 PRODUCT_COPY_FILES += \
